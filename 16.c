b7bab730b89bd2e31cc2d06d802d4470cffeb729
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-04-29 22:47:19 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -15,16 +15,8 @@ Sometimes takes time to update.
 Then likely you only contributed on 30 distinct days.
 
 Example:
+20 commits on Monday
+10 Tuesday
+20 Wednesday
 
-Sometimes takes time to update.
-
-3) If you mean 50 commits over many days but only 30 green days
-
-Then likely you only contributed on 30 distinct days.
-
-Example:
-git commit ...
-
-won’t count until:
-
-git push
+Only 3 green squares.
