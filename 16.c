7f402e24ea73fc4a0936150640bf489509c32eea
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-04-29 22:46:35 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -5,7 +5,15 @@ Enable:
 Include private con
 
 Local commits:
+Contribution graph can lag
 
+Sometimes takes time to update.
+
+3) If you mean 50 commits over many days but only 30 green days
+
+Then likely you only contributed on 30 distinct days.
+
+Example:
 git commit ...
 
 won’t count until:
