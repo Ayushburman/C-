92870923e721a394a0052864c6c2d4f2c83932e7
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-04-30 21:56:27 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -1,13 +1,4 @@
-GitHub Settings → Profile → Contributions
 
-Enable:main
-
-or merged into main.
-
-If you commit only on side branches and never merge, they may not appear.
-
-Repo may be private with private contributions hidden
-main
 
 or merged into main.
 
