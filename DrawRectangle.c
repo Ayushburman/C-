0390bae35dfd6197f1ea6c0512b01db31b1097c1
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-11 22:17:23 +0530

    Create DrawRectangle.c

---
+++ DrawRectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int size = 6;
+
+    for(i = 1; i <= size; i++) {
+        for(j = 1; j <= size; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
