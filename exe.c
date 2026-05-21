47ea59d7319fc41986ac41505b6988669e38bf77
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-21 22:13:11 +0530

    Update exe.c

--- exe.c
+++ exe.c
@@ -1,1 +1,6 @@
 
+/* 10 C Programs Used in Linux / Networking / Cybersecurity
+   These are beginner-to-intermediate level programs commonly related 
+   to Linux systems, networking, sockets, packet analysis, process 
+   handling, and cybersecurity concepts.
+*/
