96c6a3448fa151b11fa97c4f23450cc5d7b03ac8
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-21 22:09:41 +0530

    Update exe.c

--- exe.c
+++ exe.c
@@ -1,13 +1,4 @@
-STR
-    close(sock);
-int main() {
-    int sock;
-    struct sockaddr_in server;
-    char message[] = "Hello Server";
 
-    sock = socket(AF_INET, SOCK_STR
-    return 0;
-}
 
     connect(sock, (struct sockaddr *)&server, sizeof(server));
 
