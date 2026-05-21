0a6253fd47e1406f6172fa15df31ddc235ca694f
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-21 22:07:13 +0530

    Update exe.c

--- exe.c
+++ exe.c
@@ -5,7 +5,7 @@
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons(8080);
-int main() {
+int 
     int sock;
     struct sockaddr_in server;
     char message[] = "Hello Server";
