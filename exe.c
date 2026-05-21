b3354496af613a1d66fe7a99d6629091785f1bdf
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-21 22:09:05 +0530

    4

--- exe.c
+++ exe.c
@@ -1,11 +1,5 @@
 
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080);
-
     connect(sock, (struct sockaddr *)&server, sizeof(server));
 
     send(sock, message, strlen(message), 0);
