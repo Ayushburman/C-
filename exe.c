e65610e8a6884d13dc9a3896af8c00ec554475a9
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-21 22:04:19 +0530

    Update exe.c

--- exe.c
+++ exe.c
@@ -72,7 +72,12 @@ int main() {
     connect(sock, (struct sockaddr *)&server, sizeof(server));
 
     send(sock, message, strlen(message), 0);
+int main() {
+    int sock;
+    struct sockaddr_in server;
+    char message[] = "Hello Server";
 
+    sock = socket(AF_INET, SOCK_STR
     close(sock);
 int main() {
     int sock;
