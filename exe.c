12b9c96e7104793b32f0806c9d564f51c102385e
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-21 22:43:15 +0530

    Update exe.c

--- exe.c
+++ exe.c
@@ -219,7 +219,8 @@ int main() {
     return 0;
 }
 ------------------------------------
-// 10. Reverse Shell (Educational Only): Important cybersecurity concept.
+------------------------------------
+//10. Reverse Shell (Educational Only): Important cybersecurity concept.
 
    // reverse_shell.c
 #include <stdio.h>
